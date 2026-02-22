a376797df9dbfd41161c48b872661d5b3946dcdb
aiti25 <baidyaaitijhya@gmail.com>
2026-02-22 19:52:40 +0530

    Day 2

---
+++ name.c
@@ -0,0 +1,7 @@
+#include <stdio.h>   // Header file
+
+int main()           // Main function (execution starts here)
+{
+    printf("Hello, World!");   // Output statement
+    return 0;       // End of program
+}
